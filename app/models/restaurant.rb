9e7239c07da567f0ef02732eeff39b0cf9f81790
Michael Grein <michaelgrein92@gmail.com>
2017-01-12 14:58:18 -0500

    Added seed data and designed index page to display restaurants

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,13 @@
 class Restaurant < ApplicationRecord
+  CUISINES = [ "Cuisine", "American", "Mexican", "Italian", "French", "Greek", "Chinese", "Japanese", "Korean" ]
+  PRICES = [
+    ["Price", 0],
+    ["$", 1],
+    ["$$", 2],
+    ["$$$", 3],
+    ["$$$$", 4],
+    ["$$$$$", 5]
+  ]
+  attachment :menu
+  has_many :photos
 end
